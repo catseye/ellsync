2d18d4bb40caf03e7b2414bb37966f99ed0e99ec
Chris Pressey <cpressey@catseye.tc>
2018-09-25 09:12:42 +0100

    Develop tests

--- src/ellsync/tests.py
+++ src/ellsync/tests.py
@@ -13,10 +13,13 @@ class TestEllsync(unittest.TestCase):
     def setUp(self):
         super(TestEllsync, self).setUp()
         self.saved_stdout = sys.stdout
+        self.saved_stderr = sys.stderr
         sys.stdout = StringIO()
+        sys.stderr = StringIO()
         self.maxDiff = None
         check_call("rm -rf canonical cache", shell=True)
         check_call("mkdir -p canonical", shell=True)
+        check_call("touch canonical/thing", shell=True)
         check_call("mkdir -p cache", shell=True)
         router = {
             'basic': {
@@ -28,15 +31,24 @@ class TestEllsync(unittest.TestCase):
             f.write(json.dumps(router))
 
     def tearDown(self):
+        check_call("rm -rf canonical cache", shell=True)
         sys.stdout = self.saved_stdout
+        sys.stderr = self.saved_stderr
         super(TestEllsync, self).tearDown()
 
     def test_failure(self):
         with self.assertRaises(SystemExit):
             main(['backup.json'])
 
-    def test_basic(self):
+    def test_dry_run(self):
         main(['backup.json', 'canonical', 'cache'])
+        self.assertFalse(os.path.exists('cache/thing'))
+        output = sys.stdout.getvalue()
+        self.assertEqual(output, 'rsync --archive --verbose --delete canonical/ cache/\n')
+
+    def test_apply(self):
+        main(['backup.json', 'canonical', 'cache', '--apply'])
+        self.assertTrue(os.path.exists('cache/thing'))
 
 
 if __name__ == '__main__':
