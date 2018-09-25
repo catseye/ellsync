fc7cf02f419834865733a31d1fadd71f0e652227
Chris Pressey <cpressey@catseye.tc>
2018-09-25 09:07:26 +0100

    Develop tests.

--- src/ellsync/tests.py
+++ src/ellsync/tests.py
@@ -1,6 +1,9 @@
+import json
+import os
 import sys
 from StringIO import StringIO
 import unittest
+from subprocess import check_call
 
 from ellsync.main import main
 
@@ -12,6 +15,17 @@ class TestEllsync(unittest.TestCase):
         self.saved_stdout = sys.stdout
         sys.stdout = StringIO()
         self.maxDiff = None
+        check_call("rm -rf canonical cache", shell=True)
+        check_call("mkdir -p canonical", shell=True)
+        check_call("mkdir -p cache", shell=True)
+        router = {
+            'basic': {
+                'from': 'canonical',
+                'to': 'cache',
+            }
+        }
+        with open('backup.json', 'w') as f:
+            f.write(json.dumps(router))
 
     def tearDown(self):
         sys.stdout = self.saved_stdout
@@ -19,7 +33,10 @@ class TestEllsync(unittest.TestCase):
 
     def test_failure(self):
         with self.assertRaises(SystemExit):
-            main([])
+            main(['backup.json'])
+
+    def test_basic(self):
+        main(['backup.json', 'canonical', 'cache'])
 
 
 if __name__ == '__main__':
